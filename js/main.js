1b2ab317bf681af6bb802cd6e41966dade3237a3
Nikki Harper <nicoleharper@ufl.edu>
2021-03-27 12:49:35 -0600

    Add pop-up ad to display on homepage on page load

--- js/main.js
+++ js/main.js
@@ -58,6 +58,26 @@ function hideWrapper2() {
 button2.addEventListener("mouseover", showWrapper2);
 button2.addEventListener("mouseleave", hideWrapper2);
 
+//Popup ad display after 3 seconds on page load 
+var modal = document.getElementById('modal-demo');
+var closeModal = document.getElementsByClassName('close-modal')[0];
+
+window.addEventListener("load",
+  function() {
+    setTimeout(displayAd, 2000);
+  }, false
+);
+
+function displayAd() {
+  modal.classList.add('visible');
+}
+
+closeModal.addEventListener('click', 
+  function()  {
+    modal.classList.remove('visible');
+});
+
+
 
 
 
